c1b3a00c9d2b05b3286a08cdc2dfec10740f7d74
SrNaile <elianfacundofloresdecampoverde@hotmail.com>
2023-08-04 23:31:28 -0300

    complete_commit_camera_collision_and_security_system
    Sistema de cámara de seguridad activada, colisiones de la cámara y reubicación lista, juntos en un prototipo modelo.

--- Assets/Scripts/SecurityCamera/SecurityCameraRayCast.cs
+++ Assets/Scripts/SecurityCamera/SecurityCameraRayCast.cs
@@ -1,39 +1,50 @@
-/* Esta clase actúa como un detector de la cámara utilizando metodos reservados de las Physics.Ray */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRay : MonoBehaviour
+public class SecurityCameraRayCast : MonoBehaviour
 {
-    //En este update vamos a utilizar las físicas de una SphereCast para que nos ayude a detectar al personaje en un rango de visión de forma circular//
+    public SecurityCameraAlert securityCameraAlert;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
+
         RaycastHit hit;
         Vector3 p1 = transform.position;
         float distanceToObstacle = 0;
-        if (Physics.SphereCast(p1, 3.0f, transform.forward, out hit, 15f))
+        if (Physics.SphereCast(p1, 3.5f, transform.forward, out hit, 10f))
         {
             distanceToObstacle = hit.distance;
             GameObject hitObject = hit.collider.gameObject;
             if (hitObject.CompareTag("Player"))
             {
+                securityCameraAlert.OnAlert();
                 Debug.LogError("Te estoy viendo!!!!!!!!!");
+            }
+            else
+            {
+                securityCameraAlert.OffAlert();
             }
         }
     }
 
-    //Método que me permite ver en la Escena del juego el SphereCast antes de ejecutar el juego//
     private void OnDrawGizmos()
     {
         RaycastHit hit;
         Vector3 p1 = transform.position;
         float distanceToObstacle = 0;
-        if (Physics.SphereCast(p1, 3.5f, transform.forward, out hit, 15f))
+        if (Physics.SphereCast(p1, 3.5f, transform.forward, out hit, 10f))
         {
             distanceToObstacle = hit.distance;
             Gizmos.color = Color.red;
-            Gizmos.DrawSphere(hit.point, 4.0f);
+            Gizmos.DrawSphere(hit.point, 3.5f);
 
             if (hit.transform.CompareTag("Player"))
             {
