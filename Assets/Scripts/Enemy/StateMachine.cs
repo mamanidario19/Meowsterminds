33ca47d3e739ffced16575a8f43c297187393c3d
SrNaile <elianfacundofloresdecampoverde@hotmail.com>
2023-08-08 04:59:15 -0300

    Revert "Merge pull request #19 from mamanidario19/feature/add_light_and_exterior_of_level_design"
    This reverts commit bcf462c51b7bf5bfa6d07c19d29f8d0cc418fe69, reversing
    changes made to e84d821c4dfea5fdd2a744f2459b2059928ba014.

---
+++ Assets/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,29 @@
+/* En esta clase gestionaremos los estados del Enemigo*/
+using UnityEngine;
+using System.Collections;
+
+public class StateMachine : MonoBehaviour
+{
+    //Lista de variables donde establecemos las referencias a cada componente de estado
+    public State PatrolStatus; //Estado Patrulla
+    public State AlertStatus; //Estado Alerta
+    public State PersecutionStatus; //Estado Persecusion
+    public State InitialState; //Estado Inicial
+    public State EnemyAnim;
+
+    private State actualState; //Estado Actual
+    void Start()
+    {
+        ActiveState(InitialState); //Activamos estado inicial
+    }
+    
+    //Para activar un estado nuevo, le pasamos el componente que este actualmente activado
+    public void ActiveState(State newState) //Nuevo estado que queramos activar
+    {
+    if (actualState!=null) //Como estado actual no tiene valor, se ejecuta todo si es distinto de nulo
+        actualState.enabled = false; //Desactivamos el estado actual
+        actualState = newState; //Asignamos nuevo estado
+        actualState.enabled = true; //Activamos nuevo esatdo
+
+   }
+}
